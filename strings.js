01a7dc0f88e2550d9d034a48eb3d75249fef650d
neximus <liamwattley15@gmail.com>
2024-11-13 08:33:57 -0600

    fixed string in number bug

--- strings.js
+++ strings.js
@@ -11,20 +11,24 @@ let sectnece2 = "The word 'coffee' has six letters."
 //  evaluate the length of keyword and log a massage
 function evalKeyword(keyword){
     // if key word is a NOT a string;
-    if(typeof keyword !== `string` || keyword.length === 0 ){
+    if(typeof keyword !== `string` || keyword.length === 0){
         // give this message
         console.log("thats not a WORD...")
+    }else if (!isNaN(keyword)) {
+
+        console.log("sorry, "+ keyword +" has a number in it");
+
     }else{
         // otherwise give this massage
-        let evalSentence ='The word "' + keyword + '" has ' + keyword.length + " letters."; // the length should be ethat a number or a letter
+        let evalSentence ='The word "' + keyword + '" has ' + keyword.length + " letters."; //the word "keyword's stringValue" has "length of" letters
         console.log(evalSentence);
     }
 }
 
 
-evalKeyword();// no *argumant???
+evalKeyword();// no argument???
 evalKeyword("")// empty string
 evalKeyword("Pantrea Leo");// string 
 evalKeyword(280);// numbers
-evalKeyword("12345");// number in a string this. this should NOT be a word
+evalKeyword("12345");// number in a string this.
 
